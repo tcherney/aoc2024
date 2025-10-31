44a77d8dd9b8c7574297d75ce7cb115dd3db1f6c
Timothy Cherney <tjcherney@hotmail.com>
2025-10-31 00:18:08 -0700

    aoc2015 day18 from matts problem

---
+++ 2015/day18/src/game.zig
@@ -0,0 +1,288 @@
+const std = @import("std");
+const builtin = @import("builtin");
+const engine = @import("engine");
+const common = @import("common");
+const emcc = @import("emcc.zig");
+
+pub const Engine = engine.Engine;
+const GAME_LOG = std.log.scoped(.game);
+pub const TUI = engine.TUI(Game.State);
+
+const TERMINAL_HEIGHT_OFFSET = 70;
+const TERMINAL_WIDTH_OFFSET = 30;
+
+const GAME_STEPS = 100;
+
+pub const Game = struct {
+    running: bool = true,
+    e: Engine = undefined,
+    allocator: std.mem.Allocator = undefined,
+    frame_limit: u64 = 16_666_667,
+    //frame_limit: u64 = std.time.ns_per_s * 2,
+    lock: std.Thread.Mutex = undefined,
+    window: engine.Texture,
+    world: std.ArrayList(std.ArrayList(u8)) = undefined,
+    world_buffer: std.ArrayList(std.ArrayList(u8)) = undefined,
+    state: State = .start,
+    world_steps: u32 = 0,
+    tui: TUI,
+    pub const State = enum {
+        game,
+        start,
+        pause,
+    };
+    const Self = @This();
+    pub const Error = error{} || engine.Error || std.posix.GetRandomError || std.mem.Allocator.Error;
+    pub fn init(allocator: std.mem.Allocator) Error!Self {
+        return Self{
+            .allocator = allocator,
+            .window = engine.Texture.init(allocator),
+            .tui = TUI.init(allocator, .ascii),
+        };
+    }
+    pub fn deinit(self: *Self) Error!void {
+        try self.e.deinit();
+        self.window.deinit();
+        self.world.deinit();
+        self.world_buffer.deinit();
+        self.tui.deinit();
+    }
+    //TODO handle mouse/touch
+    pub fn on_mouse_change(self: *Self, mouse_event: engine.MouseEvent) void {
+        GAME_LOG.info("{any}\n", .{mouse_event});
+        _ = self;
+    }
+    pub fn on_window_change(self: *Self, win_size: engine.WindowSize) void {
+        self.lock.lock();
+        GAME_LOG.info("changed height {d}\n", .{win_size.height});
+        self.lock.unlock();
+    }
+
+    pub fn on_key_down(self: *Self, key: engine.KEYS) void {
+        GAME_LOG.info("{}\n", .{key});
+        if (key == engine.KEYS.KEY_q) {
+            self.running = false;
+        }
+        switch (self.state) {
+            .game => {
+                if (key == .KEY_w or key == .KEY_W) {} else if (key == .KEY_a or key == .KEY_A) {} else if (key == .KEY_s or key == .KEY_S) {} else if (key == .KEY_d or key == .KEY_D) {} else if (key == .KEY_q or key == .KEY_Q) {
+                    self.running = false;
+                } else if (key == .KEY_ESC) {
+                    self.state = .pause;
+                }
+            },
+            .start => {
+                if (key == .KEY_SPACE) {
+                    self.state = .game;
+                }
+            },
+            .pause => {
+                if (key == .KEY_ESC) {
+                    self.state = .game;
+                }
+            },
+        }
+    }
+
+    pub fn on_start_clicked(self: *Self) void {
+        self.state = .game;
+    }
+    var scratch_buffer: [32]u8 = undefined;
+    pub fn on_render(self: *Self, dt: u64) !void {
+        self.e.renderer.ascii.set_bg(0, 0, 0, self.window);
+        for (0..self.window.ascii_buffer.len) |i| {
+            self.window.ascii_buffer[i] = ' ';
+        }
+        _ = dt;
+        switch (self.state) {
+            .game => {
+                //GAME_LOG.info("color buffer {any}\n ascii buffer {any}", .{ self.window.pixel_buffer, self.window.ascii_buffer });
+                for (0..self.world.items.len) |i| {
+                    for (0..self.world.items[i].items.len) |j| {
+                        if (self.world.items[i].items[j] == '#') {
+                            self.e.renderer.ascii.draw_symbol(@intCast(j), @intCast(i), self.world.items[i].items[j], common.Colors.GREEN, self.window);
+                        } else {
+                            self.e.renderer.ascii.draw_symbol(@intCast(j), @intCast(i), self.world.items[i].items[j], common.Colors.WHITE, self.window);
+                        }
+                    }
+                }
+                const p = try std.fmt.bufPrint(&scratch_buffer, "Step {d} Lights {d}", .{ self.world_steps, try self.count_lights() });
+                const s_x = self.e.renderer.ascii.terminal.size.width / 2 - p.len / 2;
+                const s_y = self.e.renderer.ascii.terminal.size.height / 2;
+                for (0..p.len) |i| {
+                    self.e.renderer.ascii.draw_symbol(@intCast(s_x + i), @intCast(s_y), p[i], common.Colors.RED, self.window);
+                }
+            },
+            .start, .pause => {
+                try self.tui.draw(&self.e.renderer, self.window, 0, 0, self.state);
+            },
+        }
+        try self.e.renderer.ascii.flip(self.window, null);
+    }
+
+    pub fn em_key_handler(event_type: c_int, event: ?*const emcc.EmsdkWrapper.EmscriptenKeyboardEvent, ctx: ?*anyopaque) callconv(.C) bool {
+        GAME_LOG.info("event_type {any}\n", .{event_type});
+        GAME_LOG.info("event {any}\n", .{event});
+        const self: *Self = @ptrCast(@alignCast(ctx));
+        self.on_key_down(@enumFromInt(event.?.which));
+        return true;
+    }
+
+    pub fn update(self: *Self, part2: bool) !void {
+        if (part2) {
+            self.world_buffer.items[0].items[0] = '#';
+            self.world_buffer.items[0].items[self.world_buffer.items[0].items.len - 1] = '#';
+            self.world_buffer.items[self.world_buffer.items[0].items.len - 1].items[0] = '#';
+            self.world_buffer.items[self.world_buffer.items[0].items.len - 1].items[self.world_buffer.items[0].items.len - 1] = '#';
+        }
+        for (0..self.world.items.len) |i| {
+            for (0..self.world.items[i].items.len) |j| {
+                var num_neighbors: u8 = 0;
+                if (i > 0) {
+                    if (self.world_buffer.items[i - 1].items[j] == '#') {
+                        num_neighbors += 1;
+                    }
+                    if (j > 0) {
+                        if (self.world_buffer.items[i - 1].items[j - 1] == '#') {
+                            num_neighbors += 1;
+                        }
+                    }
+                    if (j < self.world_buffer.items[i - 1].items.len - 1) {
+                        if (self.world_buffer.items[i - 1].items[j + 1] == '#') {
+                            num_neighbors += 1;
+                        }
+                    }
+                }
+                if (j > 0) {
+                    if (self.world_buffer.items[i].items[j - 1] == '#') {
+                        num_neighbors += 1;
+                    }
+                }
+                if (j < self.world_buffer.items[i].items.len - 1) {
+                    if (self.world_buffer.items[i].items[j + 1] == '#') {
+                        num_neighbors += 1;
+                    }
+                }
+                if (i < self.world_buffer.items.len - 1) {
+                    if (self.world_buffer.items[i + 1].items[j] == '#') {
+                        num_neighbors += 1;
+                    }
+                    if (j > 0) {
+                        if (self.world_buffer.items[i + 1].items[j - 1] == '#') {
+                            num_neighbors += 1;
+                        }
+                    }
+                    if (j < self.world_buffer.items[i + 1].items.len - 1) {
+                        if (self.world_buffer.items[i + 1].items[j + 1] == '#') {
+                            num_neighbors += 1;
+                        }
+                    }
+                }
+                if (self.world_buffer.items[i].items[j] == '#') {
+                    if (num_neighbors == 2 or num_neighbors == 3) {
+                        self.world.items[i].items[j] = '#';
+                    } else {
+                        self.world.items[i].items[j] = '.';
+                    }
+                } else {
+                    if (num_neighbors == 3) {
+                        self.world.items[i].items[j] = '#';
+                    } else {
+                        self.world.items[i].items[j] = '.';
+                    }
+                }
+            }
+        }
+        if (part2) {
+            self.world.items[0].items[0] = '#';
+            self.world.items[0].items[self.world.items[0].items.len - 1] = '#';
+            self.world.items[self.world.items[0].items.len - 1].items[0] = '#';
+            self.world.items[self.world.items[0].items.len - 1].items[self.world.items[0].items.len - 1] = '#';
+        }
+        for (0..self.world.items.len) |i| {
+            for (0..self.world.items[i].items.len) |j| {
+                self.world_buffer.items[i].items[j] = self.world.items[i].items[j];
+            }
+        }
+    }
+
+    pub fn count_lights(self: *Self) !u32 {
+        var num_lights: u32 = 0;
+        for (0..self.world.items.len) |i| {
+            for (0..self.world.items[i].items.len) |j| {
+                if (self.world.items[i].items[j] == '#') {
+                    num_lights += 1;
+                }
+            }
+        }
+        return num_lights;
+    }
+
+    pub fn run(self: *Self) !void {
+        self.lock = std.Thread.Mutex{};
+        engine.set_wasm_terminal_size(35, 150);
+        self.e = try Engine.init(self.allocator, TERMINAL_WIDTH_OFFSET, TERMINAL_HEIGHT_OFFSET, .ascii, ._2d, .color_true, if (builtin.os.tag == .emscripten) .wasm else .native, if (builtin.os.tag == .emscripten) .single else .multi);
+        GAME_LOG.info("starting height {d}\n", .{self.e.renderer.ascii.terminal.size.height});
+        self.window.is_ascii = true;
+        try self.window.rect(@intCast(self.e.renderer.ascii.terminal.size.width), @intCast(self.e.renderer.ascii.terminal.size.height), 0, 0, 0, 255);
+        self.e.on_key_down(Self, on_key_down, self);
+        self.e.on_render(Self, on_render, self);
+        self.e.on_mouse_change(Self, on_mouse_change, self);
+        self.e.on_window_change(Self, on_window_change, self);
+        try self.tui.add_button(self.e.renderer.ascii.terminal.size.width / 2, self.e.renderer.ascii.terminal.size.height / 2, null, null, common.Colors.WHITE, common.Colors.BLUE, common.Colors.MAGENTA, "Start", .start);
+        self.tui.items.items[self.tui.items.items.len - 1].set_on_click(Self, on_start_clicked, self);
+        try self.tui.add_button(self.e.renderer.ascii.terminal.size.width / 2, self.e.renderer.ascii.terminal.size.height / 2, null, null, common.Colors.WHITE, common.Colors.BLUE, common.Colors.MAGENTA, "Pause", .pause);
+        self.e.set_fps(60);
+        try common.gen_rand();
+
+        self.world = std.ArrayList(std.ArrayList(u8)).init(self.allocator);
+        self.world_buffer = std.ArrayList(std.ArrayList(u8)).init(self.allocator);
+        const f = try std.fs.cwd().openFile("input.txt", .{});
+        defer f.close();
+        var buf: [65536]u8 = undefined;
+        while (try f.reader().readUntilDelimiterOrEof(&buf, '\n')) |unfiltered| {
+            var line = unfiltered;
+            if (std.mem.indexOfScalar(u8, unfiltered, '\r')) |indx| {
+                line = unfiltered[0..indx];
+            }
+            if (line.len == 0) break;
+            try self.world.append(std.ArrayList(u8).init(self.allocator));
+            try self.world_buffer.append(std.ArrayList(u8).init(self.allocator));
+            for (0..line.len) |i| {
+                try self.world.items[self.world.items.len - 1].append(line[i]);
+                try self.world_buffer.items[self.world_buffer.items.len - 1].append(line[i]);
+            }
+        }
+
+        try self.e.start();
+
+        if (builtin.os.tag == .emscripten) {
+            const res = emcc.EmsdkWrapper.emscripten_set_keydown_callback("body", self, true, em_key_handler);
+            GAME_LOG.info("handler set {d}\n", .{res});
+        }
+
+        var timer: std.time.Timer = try std.time.Timer.start();
+        var delta: u64 = 0;
+        while (self.running) {
+            delta = timer.read();
+            timer.reset();
+            self.lock.lock();
+            if (self.world_steps < GAME_STEPS) {
+                try self.update(true);
+                self.world_steps += 1;
+            }
+            self.lock.unlock();
+            delta = timer.read();
+            timer.reset();
+            if (builtin.os.tag == .emscripten) {
+                try self.on_render(delta);
+                emcc.EmsdkWrapper.emscripten_sleep(16);
+            } else {
+                const time_to_sleep: i64 = @as(i64, @bitCast(self.frame_limit)) - @as(i64, @bitCast(delta));
+                if (time_to_sleep > 0) {
+                    std.time.sleep(@as(u64, @bitCast(time_to_sleep)));
+                }
+            }
+        }
+    }
+};
